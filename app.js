159db74884b73eefae854508934db25415c9f281
Andres Hoyos <andresfabianhoyos@gmail.com>
2023-05-31 13:03:51 -0300

    update index page

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/informe-siniestros', (req, res) => {
+  res.sendFile(__dirname + '/public/informe-siniestros.html');
+});
 
 app.listen(3300, () => {
   console.log('Servidor web en ejecución en http://localhost:3300');
