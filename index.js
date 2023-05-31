7a4bf7c41dbf4ea99a5b61b04582df8c2aadec56
Andres Hoyos <andresfabianhoyos@gmail.com>
2023-05-31 14:28:11 -0300

    routes

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
+
+// app.get('/informe-siniestros', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'informe-siniestros.html'));
+// });
+
+// app.listen(process.env.PORT || 3300, () => {
+//   console.log('Servidor web en ejecución en http://localhost:3300');
+// });
+
+const express = require('express')
+
+const app = express()
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+    res.send('Express JS on Vercel')
+})
 
-app.get('/informe-siniestros', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'informe-siniestros.html'));
-});
+app.get('/ping', (req, res) => {
+    res.send('pong 🏓')
+})
 
-app.listen(process.env.PORT || 3300, () => {
-  console.log('Servidor web en ejecución en http://localhost:3300');
-});
+const port = process.env.PORT || 8080
+
+app.listen(port, (err, res) => {
+    if (err) {
+        console.log(err)
+        return res.status(500).send(err.message)
+    } else {
+        console.log('[INFO] Server Running on port:', port)
+    }
+})
