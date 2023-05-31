6d0dcbe3136fef2b7da0b571d62d0dc011c58e41
Andres Hoyos <andresfabianhoyos@gmail.com>
2023-05-31 14:05:56 -0300

    routes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-app.get('/informe-siniestros', (req, res) => {
-  res.sendFile(__dirname + '/public/informe-siniestros.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT ||3300, () => {
+app.get('/informe-siniestros', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'informe-siniestros.html'));
+});
+
+app.listen(process.env.PORT || 3300, () => {
   console.log('Servidor web en ejecución en http://localhost:3300');
 });
