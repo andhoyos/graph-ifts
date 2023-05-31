9a779090fa46a37c4231d36a8e8f01cdc850b1a4
Andres Hoyos <andresfabianhoyos@gmail.com>
2023-05-31 13:55:24 -0300

    routes

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/informe-siniestros', (req, res) => {
   res.sendFile(__dirname + '/public/informe-siniestros.html');
 });
 
-app.listen(3300, () => {
+app.listen(process.env.PORT ||3300, () => {
   console.log('Servidor web en ejecución en http://localhost:3300');
 });
