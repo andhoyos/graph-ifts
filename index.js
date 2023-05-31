d07b638acf101d25692418eb29d2b0af44b67fb2
Andres Hoyos <andresfabianhoyos@gmail.com>
2023-05-31 14:32:18 -0300

    routes

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
-// });
-
-// app.get('/informe-siniestros', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'informe-siniestros.html'));
-// });
-
-// app.listen(process.env.PORT || 3300, () => {
-//   console.log('Servidor web en ejecución en http://localhost:3300');
-// });
-
 const express = require('express')
-
+const path = require('path');
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Express JS on Vercel')
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/ping', (req, res) => {
-    res.send('pong 🏓')
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/informe-siniestros', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'informe-siniestros.html'));
+});
 
 const port = process.env.PORT || 8080
 
